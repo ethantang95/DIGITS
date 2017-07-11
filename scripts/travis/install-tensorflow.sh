a720870ab50a4c4a058072db638d57872729092a
Ethan Tang <ettang@nvidia.com>
2017-07-11 11:26:09 -0700

    whoops, wrong version number

--- scripts/travis/install-tensorflow.sh
+++ scripts/travis/install-tensorflow.sh
@@ -12,4 +12,4 @@ fi
 set -x
 
 # pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.2.0rc0-cp27-none-linux_x86_64.whl --upgrade
-pip install tensorflow==1.12.0 --upgrade
+pip install tensorflow==1.2.0 --upgrade
