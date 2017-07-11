d307d90df4332c0d7c50bdecc198f4c4d7573785
Ethan Tang <ettang@nvidia.com>
2017-07-11 14:16:58 -0700

    trying with installing protobuf from source

---
+++ scripts/travis/install-protobuf.sh
@@ -0,0 +1,29 @@
+set -e
+
+LOCAL_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ "$#" -ne 1 ];
+then
+    echo "Usage: $0 INSTALL_DIR"
+    exit 1
+fi
+
+INSTALL_DIR=$(readlink -f "$1")
+"${LOCAL_DIR}/bust-cache.sh" "$INSTALL_DIR"
+if [ -d "$INSTALL_DIR" ] && [ -e "$INSTALL_DIR/install/bin/th" ]; then
+    echo "Using cached build at $INSTALL_DIR ..."
+    exit 0
+fi
+rm -rf "$INSTALL_DIR"
+
+set -x
+
+git clone https://github.com/google/protobuf.git -b '3.3.x' /protobuf
+cd /protobuf
+./autogen.sh
+./configure --prefix=/usr
+make -j$(nproc)
+make install
+ldconfig
+cd /protobuf/python
+python setup.py install --cpp_implementation
