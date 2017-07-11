024b7d87f5150d651c8034438b31da227bb98061
Ethan Tang <ettang@nvidia.com>
2017-07-11 16:49:19 -0700

    overriding protobuf packages

--- scripts/travis/install-tensorflow.sh
+++ scripts/travis/install-tensorflow.sh
@@ -13,8 +13,12 @@ set -x
 
 sudo apt-get remove libprotobuf-dev
 
+sudo apt-get remove python-protobuf
+
 pip uninstall protobuf
 
 pip install protobuf
+
+pip show protobuf
 
 pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.2.0rc0-cp27-none-linux_x86_64.whl --upgrade
